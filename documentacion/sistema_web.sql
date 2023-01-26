f7dbce177576892bca3c2975f12ed34ac7df358d
Dan25lt <pirf860312@gmail.com>
2023-01-25 18:57:46 -0600

    lo ultimo

--- documentacion/sistema_web.sql
+++ documentacion/sistema_web.sql
@@ -1,6 +1,4 @@
---
--- Table structure for table `area_postal`
---
+
 
 CREATE TABLE `area_postal` (
   `idarea_postal` int NOT NULL AUTO_INCREMENT,
@@ -9,19 +7,7 @@ CREATE TABLE `area_postal` (
   PRIMARY KEY (`idarea_postal`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `area_postal`
---
 
-LOCK TABLES `area_postal` WRITE;
-
-INSERT INTO `area_postal` VALUES (31050,'Chihuahua','Chihuahua');
-
-UNLOCK TABLES;
-
---
--- Table structure for table `direccion`
---
 
 CREATE TABLE `direccion` (
   `iddireccion` int NOT NULL AUTO_INCREMENT,
@@ -35,18 +21,6 @@ CREATE TABLE `direccion` (
   CONSTRAINT `idarea_postal` FOREIGN KEY (`idarea_postal`) REFERENCES `area_postal` (`idarea_postal`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `direccion`
---
-
-LOCK TABLES `direccion` WRITE;
-INSERT INTO `direccion` VALUES (1,'Ochoa','1','4214','Dale',31050),(2,'Octava',NULL,'4809','Santa Rosa',31050);
-UNLOCK TABLES;
-
-
---
--- Table structure for table `cliente`
---
 
 CREATE TABLE `cliente` (
   `idcliente` int NOT NULL AUTO_INCREMENT,
@@ -61,18 +35,7 @@ CREATE TABLE `cliente` (
   CONSTRAINT `iddireccion` FOREIGN KEY (`iddireccion`) REFERENCES `direccion` (`iddireccion`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `cliente`
---
 
-LOCK TABLES `cliente` WRITE;
-INSERT INTO `cliente` VALUES (1,'Juan','Granados','juas885566sdf','6145623595','juang@gmail.com',1),(2,'karla','sepulveda','seka235689lkj','6145697812','ka@gmail.com',1),(3,'Carlos','Luna','piar124578ghj','6148235689','lucar@gmail.com',2);
-UNLOCK TABLES;
-
-
---
--- Table structure for table `empleado`
---
 
 CREATE TABLE `empleado` (
   `idempleado` int NOT NULL AUTO_INCREMENT,
@@ -84,17 +47,7 @@ CREATE TABLE `empleado` (
   PRIMARY KEY (`idempleado`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `empleado`
---
 
-LOCK TABLES `empleado` WRITE;
-INSERT INTO `empleado` VALUES (1,'Administrador','1357LOCK','Daniel','Piñon','Administrador');
-UNLOCK TABLES;
-
---
--- Table structure for table `marca`
---
 
 CREATE TABLE `marca` (
   `idmarca` int NOT NULL AUTO_INCREMENT,
@@ -103,17 +56,7 @@ CREATE TABLE `marca` (
 ) ENGINE=InnoDB;
 
 
---
--- Dumping data for table `marca`
---
 
-LOCK TABLES `marca` WRITE;
-INSERT INTO `marca` VALUES (1,'Dodge'),(2,'Chrysler'),(3,'Fiat');
-UNLOCK TABLES;
-
---
--- Table structure for table `modelo`
---
 
 CREATE TABLE `modelo` (
   `idmodelo` int NOT NULL AUTO_INCREMENT,
@@ -124,17 +67,7 @@ CREATE TABLE `modelo` (
   CONSTRAINT `idmarca` FOREIGN KEY (`idmarca`) REFERENCES `marca` (`idmarca`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `modelo`
---
 
-LOCK TABLES `modelo` WRITE;
-INSERT INTO `modelo` VALUES (1,'Charger',1),(2,'200C',2),(3,'Pulse',3);
-UNLOCK TABLES;
-
---
--- Table structure for table `vehiculos`
---
 
 CREATE TABLE `vehiculos` (
   `idvehiculos` int NOT NULL AUTO_INCREMENT,
@@ -150,18 +83,7 @@ CREATE TABLE `vehiculos` (
   CONSTRAINT `idmodelo` FOREIGN KEY (`idmodelo`) REFERENCES `modelo` (`idmodelo`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `vehiculos`
---
 
-LOCK TABLES `vehiculos` WRITE;
-INSERT INTO `vehiculos` VALUES (1,2005,'3cdertyuy12348974','EFK3700',1,1),(2,2015,'1dfdfdfdf31513132','Emg8569',2,2),(3,2020,'wedadasd6412314','EZ85654',3,1),(4,2022,'erdfsfsdf13132313','Aj15987',1,3);
-UNLOCK TABLES;
-
-
---
--- Table structure for table `operaciones`
---
 
 CREATE TABLE `operaciones` (
   `idoperaciones` int NOT NULL AUTO_INCREMENT,
@@ -169,18 +91,9 @@ CREATE TABLE `operaciones` (
   PRIMARY KEY (`idoperaciones`)
 ) ENGINE=InnoDB;
 
---
--- Dumping data for table `operaciones`
---
-
-LOCK TABLES `operaciones` WRITE;
-INSERT INTO `operaciones` VALUES (1,'Cambio de aceite'),(2,'Rotacion'),(3,'Balanceo'),(4,'Alineacion'),(5,'Limpieza de inyectores');
-UNLOCK TABLES;
 
 
---
--- Table structure for table `odendeservicio`
---
+
 
 CREATE TABLE `odendeservicio` (
   `idordenDeServicio` int NOT NULL AUTO_INCREMENT,
@@ -209,4 +122,3 @@ CREATE TABLE `odendeservicio` (
 ) ENGINE=InnoDB;
 
 
--- Dump completed on 2022-10-25 15:49:26
