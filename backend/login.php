34f417eadc8827382cbf68716e81a5848566f8fd
dotmaik1 <dotmaik1@gmail.com>
2023-01-31 21:45:21 -0600

    resto del login

--- backend/login.php
+++ backend/login.php
@@ -1,6 +1,20 @@
 <?php
     include '../config/database_config.php'; 
 
+    require_once('recaptchalib.php');
+    $privatekey = "516516541catonkeyboard351654";
+    $resp = recaptcha_check_answer ($privatekey,
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["recaptcha_challenge_field"],
+        $_POST["recaptcha_response_field"]);
+
+    if (!$resp->is_valid) {
+        // What happens when the CAPTCHA was entered incorrectly
+        die ("The reCAPTCHA wasn't entered correctly. Go back and try it again."."(reCAPTCHA said: " . $resp->error . ")");
+    } else {
+    // Your code here to handle a successful verification
+    }
+
     $responseObj = new stdClass();
 
     $username = $_POST['username'];
