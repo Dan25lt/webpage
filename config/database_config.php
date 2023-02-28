bd71fe5ccbb4f8df7b17eeb254434bb163cd7c52
dotmaik1 <dotmaik1@gmail.com>
2023-02-28 17:25:56 -0600

    Update database_config.php

--- config/database_config.php
+++ config/database_config.php
@@ -1,9 +1,9 @@
 <?php
 // Se declaran las constantes
-const DB_HOST = "localhost";
-const DB_USERNAME = "root";
+const DB_HOST = "pdb58.awardspace.net";
+const DB_USERNAME = "4196598_autocamiones";
 const DB_PASSWORD = "1357LOCK";
-const DB_DATABASE_NAME = "sistema_web";
+const DB_DATABASE_NAME = "4196598_autocamiones";
 
 $con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
 
